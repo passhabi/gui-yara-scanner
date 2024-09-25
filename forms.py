9eb49469f40ec2934b2db3e75922b07a83aa97a2
passhabi <pedramas@live.com>
2024-09-25 15:38:18 +0330

    Add the logo of Hoorad to the first form.

--- forms.py
+++ forms.py
@@ -5,23 +5,24 @@ class Form1(ctk.CTkFrame):
     def __init__(self, app):
         super().__init__(app)
         self.master = app
-        self.grid(padx=0, pady=0, sticky='nsew')
+        
+        self.grid(row=1, column=0, padx=0,  pady=0, sticky='esw')
         self.grid_rowconfigure((0,1), weight=1)
         self.grid_columnconfigure((0,1,2), weight=1)
         
         print(app)
         print(app)
-        
-        self.welcome_label = ctk.CTkLabel(self,
+                
+        welcome_label = ctk.CTkLabel(self,
                                           font=app.default_font,
                                           text="به سیستم بررسی امنیتی هووراد خوش‌امدید. برای آغاز بررسی روی کلید شروع کلیک کنید.")
-        self.welcome_label.grid(row=0, column=2, pady=12, padx=10)
+        welcome_label.grid(row=0, column=2, pady=12, padx=10)
 
-        self.btn_ready = ctk.CTkButton(self, text="شروع", command=self.master.show_form2)
-        self.btn_ready.grid(row=1, column=0, pady=12, padx=10)
+        btn_ready = ctk.CTkButton(self, text="شروع", command=self.master.show_form2)
+        btn_ready.grid(row=1, column=0, pady=12, padx=10)
         
-        self.btn_ready = ctk.CTkButton(self, text="انصراف", command=lambda: exit())
-        self.btn_ready.grid(row=1, column=1, pady=12, padx=10)
+        btn_ready = ctk.CTkButton(self, text="انصراف", command=lambda: exit())
+        btn_ready.grid(row=1, column=1, pady=12, padx=10)
 
 
 class Form2(ctk.CTkFrame):
@@ -29,8 +30,8 @@ class Form2(ctk.CTkFrame):
         super().__init__(master)
         self.master = master
 
-        self.label = ctk.CTkLabel(self, text="This is Form 2")
-        self.label.grid(row=0, column=0, pady=12, padx=10)
+        label = ctk.CTkLabel(self, text="This is Form 2")
+        label.grid(row=0, column=0, pady=12, padx=10)
 
-        self.button2 = ctk.CTkButton(self, text="Back", command=self.master.show_form1)
-        self.button2.grid(row=1, column=0, pady=12, padx=10)
+        button2 = ctk.CTkButton(self, text="Back", command=self.master.show_form1)
+        button2.grid(row=1, column=0, pady=12, padx=10)
--- main.py
+++ main.py
@@ -2,21 +2,35 @@ import customtkinter as ctk
 from tkinter import *
 from tkinter import ttk
 from forms import Form1, Form2
+from PIL import Image
 
-class UserIterface(ctk.CTk):    
-    
+
+class UserIterface(ctk.CTk):
+
     def __init__(self):
         super().__init__()
         print(self.__dir__())
-        
+
+        # treat windows, app or root as self!
         self.title("Hoorad CyberSecurity")
         self.geometry("650x200")
         self.minsize(650, 200)
         ctk.set_appearance_mode("Dark")  # default
-        self.default_font = ctk.CTkFont('Tahoma')
-        
+        self.iconbitmap("tiny.bmp")
+        self.default_font = ctk.CTkFont("Tahoma")
+
         self.columnconfigure(0, weight=1)
-        self.rowconfigure(0, weight=1)
+        self.rowconfigure((0, 1), weight=1)
+
+        logo = ctk.CTkImage(
+            dark_image=Image.open("logo.png"), light_image=Image.open("logo.png"), size=(256, 61)
+        )
+
+        logo_label = ctk.CTkLabel(
+            self, image=logo, text=""
+        )  # display image with a CTkLabel
+        
+        logo_label.grid(row=0, column=0, sticky='s')
 
         # Initialize forms.py
         self.form1 = Form1(self)
