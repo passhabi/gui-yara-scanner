3d41adb01fe66132fc8ee5b11e63214050966c8b
passhabi <pedramas@live.com>
2024-10-09 11:02:07 +0330

    Centers the gui window

--- modules/gui.py
+++ modules/gui.py
@@ -1,23 +1,24 @@
 import customtkinter as ctk
 from tkinter import *
 from tkinter import ttk
-from lib.forms import Form1, Form2
+from forms import *
 from PIL import Image
-
 
 class UserIterface(ctk.CTk):
 
     def __init__(self):
         super().__init__()
-
         # treat windows, app or root as self!
         
-        window_size = 400, 500
+        # Window Appearance:
+        window_size = 900, 670
         self.geometry(f'{window_size[0]}x{window_size[1]}') # eg."600x800"
         self.minsize(*window_size)
+        self.center_window(window_size)
+
         
         self.title("HOORAD CyberSecurity")
-        ctk.set_appearance_mode("system")  # default
+        ctk.set_appearance_mode("Dark")  # default
         self.iconbitmap("./media/tiny_icon.ico")
             
         self.default_font = ctk.CTkFont("Tahoma")
@@ -26,7 +27,7 @@ class UserIterface(ctk.CTk):
         self.rowconfigure(0, weight=1)
         self.rowconfigure(1, weight=5)
 
-        # HOORAD image:
+        # HOORAD Logo:
         logo = ctk.CTkImage(
             dark_image=Image.open("./media/logo.png"), light_image=Image.open("./media/logo.png"), size=(256, 61)
         )
@@ -43,6 +44,15 @@ class UserIterface(ctk.CTk):
 
         # Show Form 1 initially
         self.show_form1()
+        
+
+    def center_window(self, window_size):
+        width, height = window_size
+        
+        print(self.winfo_width())
+        x = (self.winfo_screenwidth() // 2) - (width // 2)
+        y = (self.winfo_screenheight() // 2) - (height // 2)
+        self.geometry(f'{width}x{height}+{x}+{y}')
 
     def show_form1(self):
         self.form2.grid_remove()
